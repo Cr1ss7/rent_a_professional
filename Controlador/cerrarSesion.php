1085100d589f24588690b0de8ec77bb218ffdc15
Cr1ss7 <79064096+Cr1ss7@users.noreply.github.com>
2021-08-24 13:54:52 -0600

    Delete Controlador directory

--- Controlador/cerrarSesion.php
+++
@@ -1,9 +0,0 @@
-<?php
-	require_once('../modelo/class.userSession.php');
-
-	$userSession = new userSession();
-	$userSession->closeSession();
-
-	header('location: ../vista/vis.inicioSesion.php');
-
-?>
