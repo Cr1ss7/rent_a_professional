49c396c37060bb21a75852484273f64548820229
Cr1ss7 <79064096+Cr1ss7@users.noreply.github.com>
2021-09-12 22:12:07 -0600

    Add files via upload

---
+++ controlador/enviarMsg.php
@@ -0,0 +1,26 @@
+<?php
+require_once('../modelo/class.conexion.php');
+require_once('../modelo/class.chat.php');
+require_once('../modelo/class.userSession.php');
+require_once('../modelo/class.cliente.php');
+require_once('../modelo/class.profecional.php');
+
+$chat = new Chat();
+$userSession = new userSession();
+$id = $_GET['id'];
+$msg = $_POST['msg'];
+
+if(isset($_SESSION['cliente'])){
+	$clnt = new Cliente();
+	$clnt->setCliente($userSession->getCurrentCliente());
+	$chat->enviarMsg($clnt->getNombre(), $msg,$id,$clnt->getId(),0);	
+	header("location: ../vistas/vis.chat.php?id=".$id);
+}elseif(isset($_SESSION['profesional'])){
+	$prof= new Profesional();
+	$prof->setProfesional($userSession->getCurrentProfesional());
+	$chat->enviarMsg($prof->getNombre(), $msg,$prof->getId(),$id,1);	
+	header("location: ../vistas/vis.chat.php?id=".$id);
+}else
+	header("location: ../vistas/vis.inicioSesion.php");
+
+?>
