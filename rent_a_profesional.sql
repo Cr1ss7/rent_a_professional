f3defffed836c6f0ba4373aba4958272aad9df9d
VReyna7 <89045210+VReyna7@users.noreply.github.com>
2021-09-12 23:09:36 -0600

    Add files via upload
    la base perra uwu

--- rent_a_profesional.sql
+++ rent_a_profesional.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.2
+-- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 13-09-2021 a las 06:12:32
--- Versión del servidor: 10.4.14-MariaDB
--- Versión de PHP: 7.4.10
+-- Tiempo de generación: 13-09-2021 a las 07:08:02
+-- Versión del servidor: 10.4.20-MariaDB
+-- Versión de PHP: 8.0.9
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -33,18 +33,31 @@ CREATE TABLE `administrador` (
   `apellido` varchar(50) NOT NULL,
   `correo` varchar(50) NOT NULL,
   `contrasena` char(40) NOT NULL,
-  `fechaNac` date NOT NULL
+  `fechaNac` date NOT NULL,
+  `contador` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Volcado de datos para la tabla `administrador`
 --
 
-INSERT INTO `administrador` (`id`, `nombre`, `apellido`, `correo`, `contrasena`, `fechaNac`) VALUES
-(14, 'Victor ', 'Reyna', 'ree@gmail.com', '698d51a19d8a121ce581499d7b701668', '2021-09-01'),
-(15, 'Cristian ', 'Pineda', 'campos@gmail.com', '698d51a19d8a121ce581499d7b701668', '2021-09-16'),
-(16, 'Angel', 'Rodriguez', 'An@gmail.com', '698d51a19d8a121ce581499d7b701668', '2021-08-30'),
-(17, 'Fernando', 'Torres', 'torres@gmail.com', '698d51a19d8a121ce581499d7b701668', '2021-09-06');
+INSERT INTO `administrador` (`id`, `nombre`, `apellido`, `correo`, `contrasena`, `fechaNac`, `contador`) VALUES
+(14, 'Victor ', 'Reyna', 'ree@gmail.com', '698d51a19d8a121ce581499d7b701668', '2021-09-01', 0),
+(15, 'Cristian ', 'Pineda', 'campos@gmail.com', '698d51a19d8a121ce581499d7b701668', '2021-09-16', 0),
+(16, 'Angel', 'Rodriguez', 'An@gmail.com', 'ce8cd0c9dac97d8d0b29b24ffbfcaba0', '2021-08-30', 1),
+(17, 'Fernando', 'Torres', 'torres@gmail.com', '698d51a19d8a121ce581499d7b701668', '2021-09-06', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `adminitradorfoto`
+--
+
+CREATE TABLE `adminitradorfoto` (
+  `id` int(11) NOT NULL,
+  `foto` varchar(5000) NOT NULL,
+  `idAdministrador` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -83,18 +96,28 @@ CREATE TABLE `cliente` (
   `apellido` varchar(20) NOT NULL,
   `correo` varchar(20) NOT NULL,
   `contrasena` varchar(40) NOT NULL,
-  `fecha_nac` date NOT NULL
+  `fecha_nac` date NOT NULL,
+  `estado` int(11) NOT NULL DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Volcado de datos para la tabla `cliente`
 --
 
-INSERT INTO `cliente` (`id`, `nombre`, `apellido`, `correo`, `contrasena`, `fecha_nac`) VALUES
-(4, 'pepe', 'setch', 'victor@gmail.com', 'b59c67bf196a4758191e42f76670ceba', '2021-08-18'),
-(5, 'Victor', 'Reyna', 'sss@gmail.com', '698d51a19d8a121ce581499d7b701668', '2021-09-06'),
-(7, 'Angel', 'SAT', 'opa@gmail.com', '310dcbbf4cce62f762a2aaa148d556bd', '2021-09-02'),
-(9, 'Ali', 'Bea', 'bea@gmail.com', '310dcbbf4cce62f762a2aaa148d556bd', '2021-09-22');
+INSERT INTO `cliente` (`id`, `nombre`, `apellido`, `correo`, `contrasena`, `fecha_nac`, `estado`) VALUES
+(18, 'Fernando', 'Rodriguez', 'ferr@gmail.com', '698d51a19d8a121ce581499d7b701668', '2021-09-01', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `clietefoto`
+--
+
+CREATE TABLE `clietefoto` (
+  `id` int(11) NOT NULL,
+  `foto` varchar(5000) NOT NULL,
+  `idCliente` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -109,17 +132,6 @@ CREATE TABLE `curri` (
   `extension` varchar(5) NOT NULL,
   `idProfesional` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Volcado de datos para la tabla `curri`
---
-
-INSERT INTO `curri` (`id`, `direccion`, `nombre`, `extension`, `idProfesional`) VALUES
-(8, '../uploads/55', 'articulo emprendedor.pdf', 'pdf', 55),
-(9, '../uploads/57', 'Cristian Omar Pineda Campos #11 2C.docx', 'docx', 57),
-(12, '../uploads/58', 'DEVELOPMENT.pdf', 'pdf', 58),
-(13, '../uploads/59', 'Naranja Negro Carros Carretera Aventura Portada de Revista.pdf', 'pdf', 59),
-(14, '../uploads/60', 'matematica 2005.pdf', 'pdf', 60);
 
 -- --------------------------------------------------------
 
@@ -136,16 +148,6 @@ CREATE TABLE `mensaje` (
   `tipo` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Volcado de datos para la tabla `mensaje`
---
-
-INSERT INTO `mensaje` (`id`, `user`, `msg`, `idProfesional`, `idCliente`, `tipo`) VALUES
-(9, 'popopopo', 'hola', 57, 9, 1),
-(10, 'Ali', 'hola', 57, 9, 0),
-(11, 'popopopo', 'como tas?', 57, 9, 1),
-(12, 'Ali', 'bien', 57, 9, 0);
-
 -- --------------------------------------------------------
 
 --
@@ -160,26 +162,29 @@ CREATE TABLE `profesional` (
   `contrasena` varchar(35) NOT NULL,
   `profesion` varchar(50) NOT NULL,
   `fecha_nac` date NOT NULL,
-  `Calificacion` decimal(5,0) DEFAULT NULL
+  `Calificacion` decimal(5,0) DEFAULT NULL,
+  `estado` int(11) NOT NULL DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Volcado de datos para la tabla `profesional`
 --
 
-INSERT INTO `profesional` (`id`, `nombre`, `apellido`, `correo`, `contrasena`, `profesion`, `fecha_nac`, `Calificacion`) VALUES
-(32, 'Cristian', 'Pineda', 'cristian@a.com', 'e58aea67b01fa747687f038dfde066f6', 'Doctor', '2021-06-30', NULL),
-(49, 'a', 'a', 'a@gmail.com', '0cc175b9c0f1b6a831c399e269772661', 'Diseñador', '2021-08-19', NULL),
-(50, 'Victor', 'reyna', 'culero@gmail.com', 'e58aea67b01fa747687f038dfde066f6', 'Desarrollador de Software', '2003-08-23', NULL),
-(51, 'r', 'r', 'r@r.com', 'b59c67bf196a4758191e42f76670ceba', 'Diseñador', '2021-08-03', NULL),
-(52, 'Victor ', 'Reynaa', 'ree@gmail.com', '934b535800b1cba8f96a5d72f72f1611', 'Desarrollador de Software', '2003-01-09', NULL),
-(54, 'ekisde', 'apellido', 'arriba@gmail.com', '698d51a19d8a121ce581499d7b701668', 'Desarrollador de Software', '2021-09-14', NULL),
-(55, 'Fernando', 'Lara', 'xd@gmail.com', '310dcbbf4cce62f762a2aaa148d556bd', 'Diseñador', '2021-09-01', NULL),
-(56, 'Fernando', 'Lara', 'xd@gmail.com', '310dcbbf4cce62f762a2aaa148d556bd', 'Diseñador', '2021-09-01', NULL),
-(57, 'popopopo', 'pipipipi', 'pipipopo@gmail.com', '4a7d1ed414474e4033ac29ccb8653d9b', 'Desarrollador de Software', '2021-09-13', NULL),
-(58, 'Ibai', 'Llanos', 'ibai@gmail.com', 'c6f057b86584942e415435ffb1fa93d4', 'Desarrollador de Software', '2021-09-20', NULL),
-(59, 'jose', 'cuervo', 'cuervo@gmail.com', 'bcbe3365e6ac95ea2c0343a2395834dd', 'Desarrollador de Software', '2021-09-14', NULL),
-(60, 'lolito', 'chido', 'lolo@gmail.com', 'b59c67bf196a4758191e42f76670ceba', 'Desarrollador de Software', '2021-09-21', NULL);
+INSERT INTO `profesional` (`id`, `nombre`, `apellido`, `correo`, `contrasena`, `profesion`, `fecha_nac`, `Calificacion`, `estado`) VALUES
+(61, 'rro', 'douuu', 'rr@gmail.com', '698d51a19d8a121ce581499d7b701668', 'Desarrollador de Software', '2021-09-01', NULL, 0),
+(62, 'Juan ', 'fernandez ', 'ju@gmail.com', '698d51a19d8a121ce581499d7b701668', 'Desarrollador de Software', '2021-09-01', NULL, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `profesionalfoto`
+--
+
+CREATE TABLE `profesionalfoto` (
+  `id` int(11) NOT NULL,
+  `foto` varchar(5000) NOT NULL,
+  `idProfesional` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -191,17 +196,17 @@ CREATE TABLE `publicaciones` (
   `id` int(11) NOT NULL,
   `titulo` varchar(50) NOT NULL,
   `descripcion` varchar(500) NOT NULL,
-  `precio` varchar(10) NOT NULL,
-  `idcliente` int(11) NOT NULL
+  `idcliente` int(11) NOT NULL,
+  `precio` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Volcado de datos para la tabla `publicaciones`
 --
 
-INSERT INTO `publicaciones` (`id`, `titulo`, `descripcion`, `precio`, `idcliente`) VALUES
-(4, 'Puta barata', 'Diler mamamela te pago con centavos', '0.01', 9),
-(5, 'Paginita', 'Quiero una paginita adonde se puedan vender menore de edad de una forma muy barata y esquivando las autoridades del pais', '1', 9);
+INSERT INTO `publicaciones` (`id`, `titulo`, `descripcion`, `idcliente`, `precio`) VALUES
+(13, 'gkkjhkjhkjhkjhkjhkjhkj', 'kjhkjhkjhkjhkjhkjhkjhkj', 18, 120),
+(14, 'xd', 'wwqdwqdwqdwqd', 18, 2147483647);
 
 -- --------------------------------------------------------
 
@@ -212,21 +217,19 @@ INSERT INTO `publicaciones` (`id`, `titulo`, `descripcion`, `precio`, `idcliente
 CREATE TABLE `reportes` (
   `Id` int(11) NOT NULL,
   `titulo` varchar(150) COLLATE utf8mb4_spanish2_ci NOT NULL,
-  `descripcion` varchar(545) COLLATE utf8mb4_spanish2_ci NOT NULL,
+  `descripcion` varchar(500) COLLATE utf8mb4_spanish2_ci NOT NULL,
+  `idClient` int(11) NOT NULL,
   `idPro` int(11) NOT NULL,
-  `idClient` int(11) NOT NULL
+  `reportado` varchar(6) COLLATE utf8mb4_spanish2_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish2_ci;
 
 --
 -- Volcado de datos para la tabla `reportes`
 --
 
-INSERT INTO `reportes` (`Id`, `titulo`, `descripcion`, `idPro`, `idClient`) VALUES
-(1, 'Epale mi piernita', 'Epico', 2, 1),
-(16, 'Epale mi piernita', 'SADSADSAD', 2, 1),
-(17, '213213', 'xzdzsdsadsad', 2, 1),
-(18, 'XD', 'epic', 2, 1),
-(19, 'XD', '123213123123', 2, 1);
+INSERT INTO `reportes` (`Id`, `titulo`, `descripcion`, `idClient`, `idPro`, `reportado`) VALUES
+(50, 'Pintura Realista2143213', '421421421421421421', 18, 61, 'Prof'),
+(52, 'ASSADADADASD', '2312321ASDASDASFSAFSAFZXZVZVZXVZ', 18, 61, 'Prof');
 
 --
 -- Índices para tablas volcadas
@@ -237,6 +240,13 @@ INSERT INTO `reportes` (`Id`, `titulo`, `descripcion`, `idPro`, `idClient`) VALU
 --
 ALTER TABLE `administrador`
   ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `adminitradorfoto`
+--
+ALTER TABLE `adminitradorfoto`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `idAdministrador` (`idAdministrador`);
 
 --
 -- Indices de la tabla `archivos`
@@ -262,19 +272,18 @@ ALTER TABLE `cliente`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indices de la tabla `clietefoto`
+--
+ALTER TABLE `clietefoto`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `idCliente` (`idCliente`);
+
+--
 -- Indices de la tabla `curri`
 --
 ALTER TABLE `curri`
   ADD PRIMARY KEY (`id`),
   ADD KEY `idProfesional` (`idProfesional`);
-
---
--- Indices de la tabla `mensaje`
---
-ALTER TABLE `mensaje`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `idProfesional` (`idProfesional`),
-  ADD KEY `idCliente` (`idCliente`);
 
 --
 -- Indices de la tabla `profesional`
@@ -283,18 +292,26 @@ ALTER TABLE `profesional`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indices de la tabla `profesionalfoto`
+--
+ALTER TABLE `profesionalfoto`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `idProfesional` (`idProfesional`);
+
+--
 -- Indices de la tabla `publicaciones`
 --
 ALTER TABLE `publicaciones`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `nomProfesion` (`precio`),
   ADD KEY `idcliente` (`idcliente`);
 
 --
 -- Indices de la tabla `reportes`
 --
 ALTER TABLE `reportes`
-  ADD PRIMARY KEY (`Id`);
+  ADD PRIMARY KEY (`Id`),
+  ADD KEY `idClient` (`idClient`),
+  ADD KEY `idPro` (`idPro`);
 
 --
 -- AUTO_INCREMENT de las tablas volcadas
@@ -322,37 +339,31 @@ ALTER TABLE `chat`
 -- AUTO_INCREMENT de la tabla `cliente`
 --
 ALTER TABLE `cliente`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
 
 --
 -- AUTO_INCREMENT de la tabla `curri`
 --
 ALTER TABLE `curri`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
-
---
--- AUTO_INCREMENT de la tabla `mensaje`
---
-ALTER TABLE `mensaje`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT de la tabla `profesional`
 --
 ALTER TABLE `profesional`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=61;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=63;
 
 --
 -- AUTO_INCREMENT de la tabla `publicaciones`
 --
 ALTER TABLE `publicaciones`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
 -- AUTO_INCREMENT de la tabla `reportes`
 --
 ALTER TABLE `reportes`
-  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=53;
 
 --
 -- Restricciones para tablas volcadas
@@ -383,6 +394,13 @@ ALTER TABLE `curri`
 --
 ALTER TABLE `publicaciones`
   ADD CONSTRAINT `publicaciones_ibfk_1` FOREIGN KEY (`idcliente`) REFERENCES `cliente` (`id`);
+
+--
+-- Filtros para la tabla `reportes`
+--
+ALTER TABLE `reportes`
+  ADD CONSTRAINT `reportes_ibfk_1` FOREIGN KEY (`idPro`) REFERENCES `profesional` (`id`),
+  ADD CONSTRAINT `reportes_ibfk_2` FOREIGN KEY (`idClient`) REFERENCES `cliente` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
